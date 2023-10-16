c822a2c517762a8fdc9dfdbc39937a6732d60be7
Gustavo Muñoz <timbergus@gmail.com>
2023-10-16 23:01:48 +0200

    Move to cmake.

--- Makefile
+++ Makefile
@@ -1,35 +1,50 @@
-CXX=clang++-13
+# # CXX=clang++-13
+# CXX=clang++
 
-TARGET=bbb-playground
+# TARGET=bbb-playground
 
-ROOT=src
-APP=$(ROOT)/app
-INCLUDE=$(ROOT)/include
-BIN=bin
+# ROOT=src
+# APP=$(ROOT)/app
+# INCLUDE=$(ROOT)/include
+# BIN=bin
 
-CFLAGS=-std=c++2b -Werror -Wall -Wextra
-IFLAGS=-I $(INCLUDE) -I /usr/local/include
-LFLAGS=-L /usr/local/lib -l:librobotcontrol.so.1
+# CFLAGS=-std=c++2b -Werror -Wall -Wextra
+# IFLAGS=-I $(INCLUDE) -I /usr/local/include
+# LFLAGS=-L /usr/local/lib # -l:librobotcontrol.so.1
 
-OBJS=$(BIN)/main.o
+# OBJS=$(BIN)/main.o
 
-$(BIN)/%.o: $(INCLUDE)/%.cpp
-	@mkdir -p $(BIN)
-	$(CXX) $(CFLAGS) $(IFLAGS) -c -MD $< -o $@
+# $(BIN)/%.o: $(INCLUDE)/%.cpp
+# 	@mkdir -p $(BIN)
+# 	$(CXX) $(CFLAGS) $(IFLAGS) -c -MD $< -o $@
 
-$(BIN)/%.o: $(APP)/%.cpp
-	@mkdir -p $(BIN)
-	$(CXX) $(CFLAGS) $(IFLAGS) -c -MD $< -o $@
+# $(BIN)/%.o: $(APP)/%.cpp
+# 	@mkdir -p $(BIN)
+# 	$(CXX) $(CFLAGS) $(IFLAGS) -c -MD $< -o $@
 
-$(TARGET): $(OBJS)
-	@$(CXX) $(CFLAGS) $(IFLAGS) $(LFLAGS) $(OBJS) -o $(BIN)/$(TARGET)
+# $(TARGET): $(OBJS)
+# 	@$(CXX) $(CFLAGS) $(IFLAGS) $(LFLAGS) $(OBJS) -o $(BIN)/$(TARGET)
 
--include $(BIN)/*.d
+# -include $(BIN)/*.d
+
+# start:
+# 	@$(BIN)/$(TARGET)
+
+# .PHONY: clean
+
+# clean:
+# 	rm -r $(BIN)
+
+BUILD=build
+
+init:
+	cmake -B $(BUILD)
+
+build_project:
+	cmake --build $(BUILD)
 
 start:
-	@$(BIN)/$(TARGET)
-
-.PHONY: clean
+	./$(BUILD)/bbb-playground
 
 clean:
-	rm -r $(BIN)
+	rm -rf $(BUILD)
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,39 +3,39 @@
 #include <ctime>
 #include <signal.h>
 
-#include "board.h"
+// #include "board.h"
 
-void capture_sample()
+/* void capture_sample()
 {
   time_t now = time(0);
-  
+
   std::string file_name("./src/data/" + std::to_string(now) + ".txt");
-  
+
   std::ofstream file(file_name);
-  
+
   Board board(2, 9600);
-  
+
   board.gps_init();
-  
+
   auto start = std::chrono::high_resolution_clock::now();
-  
+
   for (int i = 0; i < 10000; i++)
   {
     // std::cout << board.gps_read();
-    
+
     file << board.gps_read();
   }
-  
+
   auto finish = std::chrono::high_resolution_clock::now();
-  
+
   std::chrono::duration<double> elapsed = finish - start;
 
   std::cout << "Elapsed time: " << elapsed.count() << " s\n";
-  
+
   board.gps_close();
-  
+
   file.close();
-  
+
   rc_led_set(RC_LED_RED, 1);
   rc_led_set(RC_LED_GREEN, 0);
 }
@@ -58,33 +58,36 @@ void __on_pause_release(void)
   return;
 }
 
-void __signal_handler(__attribute__ ((unused)) int dummy)
+void __signal_handler(__attribute__((unused)) int dummy)
 {
   running = 0;
   return;
-}
+} */
 
 int main()
 {
-  rc_led_set(RC_LED_RED, 1);
+  /* rc_led_set(RC_LED_RED, 1);
   rc_led_set(RC_LED_GREEN, 0);
-  
-  if(rc_button_init(RC_BTN_PIN_PAUSE, RC_BTN_POLARITY_NORM_HIGH, RC_BTN_DEBOUNCE_DEFAULT_US))
+
+  if (rc_button_init(RC_BTN_PIN_PAUSE, RC_BTN_POLARITY_NORM_HIGH, RC_BTN_DEBOUNCE_DEFAULT_US))
   {
-    fprintf(stderr,"ERROR: failed to init buttons\n");
+    fprintf(stderr, "ERROR: failed to init buttons\n");
     return -1;
   }
-  
+
   signal(SIGINT, __signal_handler);
   running = 1;
-  
+
   rc_button_set_callbacks(RC_BTN_PIN_PAUSE, __on_pause_press, __on_pause_release);
-  
-  while(running) {
+
+  while (running)
+  {
     rc_usleep(500000);
   }
-  
-  rc_button_cleanup();
+
+  rc_button_cleanup(); */
+
+  std::cout << "Hello, world!" << std::endl;
 
   return EXIT_SUCCESS;
 }
